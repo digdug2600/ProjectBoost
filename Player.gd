c4401e0f3d49e0b78433df95770b55b9c809123c
Doug Brozek <dougbrozek@yahoo.com>
2026-02-11 23:29:56 -0500

    added collision to rigid bodies

--- Player.gd
+++ Player.gd
@@ -5,7 +5,7 @@ extends RigidBody3D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_accept"):
-		apply_central_force(Vector3.UP * delta * 1000.0)
+		apply_central_force(basis.y * delta * 1000.0)
 
 	if Input.is_action_pressed("ui_left"):
 		apply_torque(Vector3(0.0, 0.0, 100.0 * delta))

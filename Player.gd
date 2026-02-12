de2a7e8000a752297a0d4182c4ea34025e1ebac2
Doug Brozek <dougbrozek@yahoo.com>
2026-02-11 23:42:52 -0500

    Key mappings

--- Player.gd
+++ Player.gd
@@ -4,11 +4,11 @@ extends RigidBody3D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_accept"):
+	if Input.is_action_pressed("Boost"):
 		apply_central_force(basis.y * delta * 1000.0)
 
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("Left Rotate"):
 		apply_torque(Vector3(0.0, 0.0, 100.0 * delta))
 
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("Rotate Right"):
 		apply_torque(Vector3(0.0, 0.0, -100.0 * delta))

3a1cfb85c8315467c5146cc8fe71fb7bfd523e82
Doug Brozek <dougbrozek@yahoo.com>
2026-02-16 00:36:44 -0500

    Added respawn

--- Player.gd
+++ Player.gd
@@ -1,21 +1,27 @@
 extends RigidBody3D
+## How much vertical force to apply when moving.
+@export_range(750.0, 3000.0) var thurst: float = 1000.0
 
-
+@export var torque_thrust: float = 100.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("Boost"):
-		apply_central_force(basis.y * delta * 1000.0)
+		apply_central_force(basis.y * delta * thurst)
 
 	if Input.is_action_pressed("Left Rotate"):
-		apply_torque(Vector3(0.0, 0.0, 100.0 * delta))
+		apply_torque(Vector3(0.0, 0.0, torque_thrust * delta))
 
 	if Input.is_action_pressed("Rotate Right"):
-		apply_torque(Vector3(0.0, 0.0, -100.0 * delta))
+		apply_torque(Vector3(0.0, 0.0, -torque_thrust * delta))
 
 func _on_body_entered(body: Node) -> void:
 	if "Goal" in body.get_groups():
 		print("You Win!")
 		
 	if "Hazard" in body.get_groups():
-		print("You Crashed!")
+		crash_sequence()
+		
+func crash_sequence() -> void:
+	print("KABOOM!")
+	get_tree().reload_current_scene()
